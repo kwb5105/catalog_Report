d7aba5e4ff31ce27960b097b5fb7a05f06540b61
Kyle Burger <kyle.W.burger@ge.com>
2018-10-03 20:37:13 -0500

    Updating Python code to meet PEP8 standards

--- catalog.py
+++ catalog.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# 
 # web service for displaying Catalog reports.
 
 from flask import Flask, request, redirect, url_for
@@ -34,28 +33,40 @@ HTML_WRAP = '''\
 
 # HTML template for an individual article
 ARTICLES = '''\
-    <div class=catalog><strong class=ArticleType>Article: %s</strong><br>Views: %s</div>
+    <div class=catalog><strong class=ArticleType>Article: \
+    %s </strong><br>Views: %s</div>
 '''
 # HTML template for an individual author
 AUTHORS = '''\
-    <div class=catalog><strong class=AuthorType>Author: %s</strong><br>Views: %s</div>
+    <div class=catalog><strong class=AuthorType>Author: \
+    %s </strong><br>Views: %s</div>
 '''
 # HTML template for an error result lists
 ERRORLOG = '''\
-    <div class=catalog><strong class=errorLogType>Day: %s</strong><br>Error Percent: %s</div>
+    <div class=catalog><strong class=errorLogType>Day: \
+    %s</strong><br>Error Percent: %s</div>
 '''
+
 
 @app.route('/', methods=['GET'])
 def main():
-  '''Top Article Report'''
-  results = "<h1>Top Articles</h1>"
-  results = results + "".join(ARTICLES % (article, views) for views, article in get_top_articles())
-  results = results + "<h1>Top Authors</h1>"
-  results = results + "".join(AUTHORS % (author, views) for views, author in get_top_authors())
-  results = results + "<h1>Days with more than 1 percent request errors</h1>"
-  results = results + "".join(ERRORLOG % (date, failedPercentage) for failedPercentage, date in get_top_errors())
-  html = HTML_WRAP % results
-  return html
+    '''Top Article Report'''
+    results = "<h1>Top Articles</h1>"
+    results = results + "".join(
+        ARTICLES % (article, views) for views, article in get_top_articles()
+            )
+    results = results + "<h1>Top Authors</h1>"
+    results = results + "".join(
+        AUTHORS % (author, views) for views, author in get_top_authors()
+        )
+    results = results + "<h1>Days with more than 1 percent request errors</h1>"
+    results = results + "".join(
+        ERRORLOG % (
+            date, failedPercentage
+            ) for failedPercentage, date in get_top_errors()
+        )
+    html = HTML_WRAP % results
+    return html
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
--- catalogdb.py
+++ catalogdb.py
@@ -1,32 +1,51 @@
 # Database code for the DB catalog.
 
-import psycopg2 
-
+import psycopg2
 DBNAME = "news"
 
+
 def get_top_articles():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("select count(log.id) as Views, articles.title from log left join articles on (log.path like CONCAT('%', articles.slug, '%')) WHERE status = '200 OK' and title != '' GROUP BY articles.title order by count(log.id) desc LIMIT 3")
-  articles = c.fetchall()
-  db.close()
-  return articles
+    """Return all posts from the 'database', most recent first."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select count(log.id) as Views, articles.title \
+    from log left join articles on \
+    (log.path like CONCAT('%', articles.slug, '%')) \
+    WHERE status = '200 OK' and title != '' GROUP BY articles.title \
+    order by count(log.id) desc LIMIT 3")
+    articles = c.fetchall()
+    db.close()
+    return articles
+
 
 def get_top_authors():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("select count(log.id) as Views, authors.name from log left join articles on (log.path like CONCAT('%', articles.slug, '%')) inner join authors on (authors.id = articles.author) WHERE status = '200 OK' and title != '' GROUP BY authors.name order by count(log.id) desc")
-  authors = c.fetchall()
-  db.close()
-  return authors
+    """Return all posts from the 'database', most recent first."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select count(log.id) as Views, authors.name \
+    from log left join articles on (log.path like \
+    CONCAT('%', articles.slug, '%')) inner join authors \
+    on (authors.id = articles.author) WHERE status = '200 OK' \
+    and title != '' GROUP BY authors.name \
+    order by count(log.id) desc")
+    authors = c.fetchall()
+    db.close()
+    return authors
+
 
 def get_top_errors():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("select round((round(failedCount.count::decimal,2)/round(logDateCount.count::decimal,2) *100)::decimal,2) as failedPercentage, failedCount.time from failedCount join logDateCount on (failedCount.time = logDateCount.time) where round((round(failedCount.count::decimal,2)/round(logDateCount.count::decimal,2) *100)::decimal,2) > 1 order by failedPercentage desc")
-  topErrors = c.fetchall()
-  db.close()
-  return topErrors
+    """Return all posts from the 'database', most recent first."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select round((round\
+    (failedCount.count::decimal,2)/round\
+    (logDateCount.count::decimal,2) *100)::decimal,2) \
+    as failedPercentage, failedCount.time \
+    from failedCount join logDateCount on \
+    (failedCount.time = logDateCount.time) \
+    where round((round(failedCount.count::decimal,2)\
+    /round(logDateCount.count::decimal,2) *100)::decimal,2) > \
+    1 order by failedPercentage desc")
+    topErrors = c.fetchall()
+    db.close()
+    return topErrors
